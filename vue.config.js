66af07ea241c37ab44ba056829918dfd06c02430
sawyerbutton <linghui92liu@gmail.com>
2024-08-16 18:17:48 +0800

    update config file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
+// 基本路径根据当前是生产环境还是开发环境来判断
+let BASE_URL = process.env.NODE_ENV === 'production' ? '/revenue-calculator/' : '/'
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: BASE_URL
 })
